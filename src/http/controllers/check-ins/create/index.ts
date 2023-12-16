8d3685efdcc5ba197bfb3d2b4cafd3e05a4bdb91
Raiane <raiane.oliveira404@gmail.com>
2023-12-16 11:51:14 -0300

    feat: missing controllers and e2e test

---
+++ src/http/controllers/check-ins/create/index.ts
@@ -0,0 +1,33 @@
+import { makeCheckInUseCase } from '@/use-cases/@factories/make-check-in-use-case'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+export async function create(request: FastifyRequest, reply: FastifyReply) {
+  const createCheckInParamsSchema = z.object({
+    gymId: z.string().uuid(),
+  })
+
+  const createCheckInBodySchema = z.object({
+    userLatitude: z.number().refine((value) => {
+      return Math.abs(value) <= 90
+    }),
+    userLongitude: z.number().refine((value) => {
+      return Math.abs(value) <= 180
+    }),
+  })
+
+  const { gymId } = createCheckInParamsSchema.parse(request.params)
+  const { userLatitude, userLongitude } = createCheckInBodySchema.parse(
+    request.body,
+  )
+
+  const checkInUseCase = makeCheckInUseCase()
+  await checkInUseCase.execute({
+    userId: request.user.sub,
+    gymId,
+    userLatitude,
+    userLongitude,
+  })
+
+  return reply.status(201).send()
+}
